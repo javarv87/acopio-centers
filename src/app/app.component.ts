7f8f1f2716eec7dbb82ab92a26b08fa93f4956cb
Javier Ruiz <javier.ruiz@mobilepro.mx>
2017-09-25 10:18:36 -0500

    Adding responsive sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild, OnInit, HostListener } from '@angular/core';
+import { MdSidenav } from '@angular/material';
 
 @Component({
   moduleId: module.id,
@@ -7,13 +8,31 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./app.component.sass'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('sidenav') sidenav: MdSidenav;
+  navMode = 'side';
   folders = [
-    {
-      name: 'Centros'
-    },
-    {
-      name: 'Productos'
-    }
+    { name: 'Centros' },
+    { name: 'Productos' }
   ];
+
+  constructor() {}
+
+  ngOnInit() {
+    if (window.innerWidth < 768) {
+      this.navMode = 'over';
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+    onResize(event) {
+      if (event.target.innerWidth < 768) {
+        this.navMode = 'over';
+        this.sidenav.close();
+      }
+      if (event.target.innerWidth > 768) {
+        this.navMode = 'side';
+        this.sidenav.open();
+      }
+    }
 }
